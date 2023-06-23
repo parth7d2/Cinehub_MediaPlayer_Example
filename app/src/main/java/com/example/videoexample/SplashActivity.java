14c8f7227e7193c7ff003da2810dc2edc2fa5cfb
Parth7d2 <parth.comengineer@gmail.com>
2023-06-23 20:24:57 +0530

    First commit after MediaPlayer Basic Features are complete with ExoPlayer library.

---
+++ app/src/main/java/com/example/videoexample/SplashActivity.java
@@ -0,0 +1,28 @@
+package com.example.videoexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+
+        Intent iHome = new Intent(SplashActivity.this, MainActivity.class);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(iHome);
+                finish();
+            }
+        },1000);
+    }
+}
